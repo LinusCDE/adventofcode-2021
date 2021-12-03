1c106a51ff9910cb8db9a2ff15707525cdf07a33
Linus <linus@cosmos-ink.net>
2021-12-03 15:34:27 +0100

    Add rust version of day 1

---
+++ rust/src/day1.rs
@@ -0,0 +1,52 @@
+pub fn solve_part_1(input: &str) -> anyhow::Result<usize> {
+    let mut increments: usize = 0;
+    let mut last_measurement: Option<u64> = None;
+
+    for line in input.split('\n').map(|s| s.trim()) {
+        if line.is_empty() {
+            continue;
+        }
+        let measurement = line.parse()?;
+
+        if let Some(last_measurement) = last_measurement {
+            if measurement > last_measurement {
+                increments += 1;
+            }
+        }
+        last_measurement = Some(measurement);
+    }
+    Ok(increments)
+}
+
+pub fn solve_part_2(input: &str) -> anyhow::Result<usize> {
+    let mut increments: usize = 0;
+    let mut last_measurement_sum: Option<u64> = None;
+    let mut window = (None::<u64>, None::<u64>, None::<u64>);
+
+    for line in input.split('\n').map(|s| s.trim()) {
+        if line.is_empty() {
+            continue;
+        }
+        let measurement = line.parse()?;
+
+        window.0 = window.1;
+        window.1 = window.2;
+        window.2 = Some(measurement);
+
+        let measurement_sum = match window {
+            (Some(val1), Some(val2), Some(val3)) => Some(val1 + val2 + val3),
+            _ => None,
+        };
+
+        if_chain! {
+            if let Some(last_measurement_sum) = last_measurement_sum;
+            if let Some(measurement_sum) = measurement_sum;
+            if measurement_sum > last_measurement_sum;
+            then {
+                increments += 1;
+            }
+        }
+        last_measurement_sum = measurement_sum;
+    }
+    Ok(increments)
+}
---
+++ rust/src/main.rs
@@ -0,0 +1,65 @@
+#[macro_use]
+extern crate log;
+
+#[macro_use]
+extern crate if_chain;
+
+use anyhow::{Context, Result};
+use clap::{crate_authors, crate_version, Parser};
+use std::{env, fs, path::PathBuf, process::exit, time::Instant};
+
+mod day1;
+
+#[derive(Debug, Parser)]
+#[clap(version = crate_version!(), author = crate_authors!())]
+struct Opts {
+    #[clap(long, short = 'f', about = "Specifiy another input file to use")]
+    input_file: Option<PathBuf>,
+
+    #[clap(about = "What day to solve")]
+    day: usize,
+    #[clap(about = "What part to solve")]
+    part: usize,
+}
+
+fn main() -> Result<()> {
+    // Parse cli
+    let opts = Opts::parse();
+
+    // Init logging (default to info)
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "INFO");
+    }
+    env_logger::init();
+
+    // Basic bound checks
+    if opts.day < 1 || opts.day > 31 {
+        error!("Invalid day \"{}\" Only 1-31 are allowed!", opts.day);
+        exit(1);
+    }
+    if opts.part != 1 && opts.part != 2 {
+        error!("Invalid part \"{}\" Only 1 and 2 are allowed!", opts.part);
+        exit(1);
+    }
+
+    let filename = opts
+        .input_file
+        .unwrap_or(PathBuf::from(format!("../input/day{}.txt", opts.day)));
+    let filecontent = fs::read_to_string(filename).context("Reading input file")?;
+
+    let start = Instant::now();
+    let solution: String = match opts.day {
+        1 => match opts.part {
+            1 => day1::solve_part_1(&filecontent).map(|v| v.to_string()),
+            2 => day1::solve_part_2(&filecontent).map(|v| v.to_string()),
+            _ => unreachable!(),
+        },
+        _ => {
+            unimplemented!()
+        }
+    }
+    .with_context(|| format!("Solving day {} part {}", opts.day, opts.part))?;
+
+    info!("Solved in {:?}: {}", start.elapsed(), solution);
+    Ok(())
+}
