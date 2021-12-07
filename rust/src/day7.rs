fe78f5b5d105af65c9d8e78e009b16cb4d863696
Linus <linus@cosmos-ink.net>
2021-12-07 13:43:01 +0100

    Speed up calculation by using a triangular numbers
    https://stackoverflow.com/a/10488466

--- rust/src/day7.rs
+++ rust/src/day7.rs
@@ -1,26 +1,26 @@
-fn min_fuel<F>(input: &str, fuel_calc: F) -> anyhow::Result<usize>
+fn min_fuel<F>(input: &str, fuel_calc: F) -> anyhow::Result<isize>
 where
-    F: Fn(i16, i16) -> usize,
+    F: Fn(isize) -> isize,
 {
     let positions = input
         .trim()
         .split(',')
         .map(|word| word.parse())
-        .collect::<Result<Vec<i16>, _>>()?;
+        .collect::<Result<Vec<isize>, _>>()?;
     ensure!(positions.len() > 0, "No positions found!");
 
     // Iter from min to incl. max horizontal postition and find lowest
     // sum of all fuel calculations to that point.
     (*positions.iter().min().unwrap()..=*positions.iter().max().unwrap())
-        .map(|t| positions.iter().map(|p| fuel_calc(*p, t)).sum())
+        .map(|t| positions.iter().map(|p| fuel_calc((t - *p).abs())).sum())
         .min()
         .ok_or(anyhow!("No minimum fuel found!"))
 }
 
 pub fn solve_part_1(input: &str) -> anyhow::Result<impl std::fmt::Display> {
-    min_fuel(input, |p, t| (t - p).abs() as usize)
+    min_fuel(input, |distance| distance)
 }
 
 pub fn solve_part_2(input: &str) -> anyhow::Result<impl std::fmt::Display> {
-    min_fuel(input, |p, t| (1..=(t - p).abs() as usize).sum())
+    min_fuel(input, |distance| distance * (distance + 1) / 2)
 }
